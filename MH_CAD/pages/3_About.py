577746d1c378e7e8093d82e501cc3b21e5a03b3a
MecHub <144913824+GitMechub@users.noreply.github.com>
2025-05-29 08:52:01 -0300

    Update 3_About.py

--- MH_CAD/pages/3_About.py
+++ MH_CAD/pages/3_About.py
@@ -31,16 +31,16 @@ if 'image_ok' in st.session_state:
     except:
         pass
 
-st.header("MecHub CAD v1.0.0", divider="gray", anchor=False)
+st.header("MecHub CAD v2.0.0", divider="gray", anchor=False)
 
 st.markdown('''
 ## About MecHub CAD:
 
-MecHub CAD is a simple application designed to help engineers, designers, and hobbyists transform coordinate data and images into 3D models through extrusion.
+MecHub CAD is a simple application designed to help engineers, designers, and hobbyists transform coordinate data, images, or messages into 3D models through extrusion.
 
 ## How it Works:
 
-MecHub CAD offers two primary functions:
+MecHub CAD offers three primary functions:
 
 1. **Coordinate-Based 3D Extrusion:**  
    Upload a file containing a series of (x, y) coordinates representing a contour. MecHub CAD will extrude this contour into a fully defined 3D shape, ready for CAD modeling and further processing.
@@ -48,15 +48,19 @@ MecHub CAD offers two primary functions:
 2. **Image-Based 3D Extrusion:**  
    Upload a .jpg, .jpeg, or .png image containing an external contour. MecHub CAD will process the image and extract the contour, generating a corresponding 3D model through extrusion.
 
+3. **Prompt-Based 3D Extrusion via AI Chatbot:** **(NEW):**   
+   Enter a prompt into the AI chatbot powered by Google Gemini. MecHub CAD will interpret the message and automatically generate a 3D model based on the described object contour.
+
 ## Key Features:
 
 - **Coordinate File Support:** Compatible with .xlsx and .csv formats for contour generation.
 - **Image Contour Extraction:** Supports common image formats (.jpg, .jpeg, .png) for smooth contour identification.
+- **(NEW)****Prompt-to-Model AI Integration:** Create 3D models directly from descriptive text prompts using Google Gemini.  
 - **Precise Extrusion:** Easily generate complex 3D models from contours.
 - **CAD Integration:** Export clean, ready-to-use models for CAD software.
 - **User-Friendly Interface:** Simplifies the transformation from 2D to 3D with minimal input requirements.
 
-MecHub CAD makes it simple to turn 2D representations, whether from coordinate files or image contours, into 3D models through intuitive extrusion tools.
+MecHub CAD makes it simple to turn 2D representations, whether from coordinate files, image contours, or AI chatbot prompts, into 3D models through intuitive extrusion tools.
 
   ---
   '''
